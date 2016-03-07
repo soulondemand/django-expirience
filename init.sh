ea3d1b450a6cab2c93c81ec79ac9b5d3e032cf39
Alexandr Smirnov <buyer@mail.ru>
2016-03-07 09:22:54 +0000

    remove nginx default site.

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #/etc/nginx/nginx.conf - config nginx
-sudo ln -sf /home/alexandr/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
