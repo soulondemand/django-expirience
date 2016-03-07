00864b2cfbc2d8de7b321c7043d724af3d4ce430
Alexandr Smirnov <buyer@mail.ru>
2016-03-07 14:42:10 +0500

    add nginx log. Path - local. Ok

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #/etc/nginx/nginx.conf - config nginx
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/alexandr/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
