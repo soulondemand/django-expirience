be8bbabef0de32e123c97e85591eba672fb2c3a1
Alexandr Smirnov <buyer@mail.ru>
2016-03-07 15:11:02 +0500

    add gunicorn: config file, restart daemon by init.sh. path - local.

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #/etc/nginx/nginx.conf - config nginx
-sudo rm /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -sf /home/alexandr/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/alexandr/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
