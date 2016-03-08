9d38d9b29b9c462f85c6e4d1d12790f1817662ef
Alexandr Smirnov <buyer@mail.ru>
2016-03-08 15:42:22 +0000

    change path to Stepic

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@
 #/etc/nginx/nginx.conf - config nginx
 sudo rm -f /etc/nginx/sites-enabled/default
 #sudo kill `pidof python`
-sudo ln -sf /home/alexandr/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/alexandr/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 #gunicorn -D -b 0.0.0.0:8080 --workers=2 hello:app
