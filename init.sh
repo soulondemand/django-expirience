db48619b5f9e17ff9b521b52bdd6b81ffc7a2ac5
Alexandr Smirnov <buyer@mail.ru>
2016-03-09 06:04:36 +0000

    add gunicorn to project 'ask'

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ sudo rm -f /etc/nginx/sites-enabled/default
 #sudo kill `pidof python`
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.test.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.ask.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 #gunicorn -D -b 0.0.0.0:8080 --workers=2 hello:app
