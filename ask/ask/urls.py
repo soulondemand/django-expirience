0d9aa1fab9aabd0bbf41cc92e036966ee9e265d7
Alexandr Smirnov <buyer@mail.ru>
2016-03-09 06:23:13 +0000

    ask manage validate. runtime exception: import qa

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from ask import qa
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
@@ -12,7 +13,7 @@ urlpatterns = patterns('',
     url(r'^$', include(admin.site.urls)),
     url(r'^login/', include(admin.site.urls)),
     url(r'^signup/', include(admin.site.urls)),
-    url(r'^question/(?P<question_id>[^/]+)/', include(qa.views.test))),
+    #url(r'^question/(?P<question_id>[^/]+)/', include(qa.views.test)),
     url(r'^ask/', include(admin.site.urls)),
     url(r'^popular/', include(admin.site.urls)),
     url(r'^new/', include(admin.site.urls)),
