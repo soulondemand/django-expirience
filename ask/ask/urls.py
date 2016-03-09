f8d7d5177cfedd687cc8a99281aac6495a7805ba
Smirnov Alexandr <buyer@mail.ru>
2016-03-09 08:40:06 +0000

    localhost: Ok. It is work. bla-bla-bla... But urls unconfigured

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,18 +3,21 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from ask import qa
+#from ask import qa
+import qa
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(admin.site.urls)),
-    url(r'^login/', include(admin.site.urls)),
-    url(r'^signup/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', include(admin.site.urls)),
+    #url(r'^login/', include(admin.site.urls)),
+    #url(r'^signup/', include(admin.site.urls)),
     #url(r'^question/(?P<question_id>[^/]+)/', include(qa.views.test)),
-    url(r'^ask/', include(admin.site.urls)),
-    url(r'^popular/', include(admin.site.urls)),
-    url(r'^new/', include(admin.site.urls)),
+    #url(r'^question/', include(qa.views.test)),
+    #url(r'^ask/', include(admin.site.urls)),
+    #url(r'^popular/', include(admin.site.urls)),
+    #url(r'^new/', include(admin.site.urls)),
 )
