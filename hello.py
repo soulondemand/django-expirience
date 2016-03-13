00307e5d7f922dad8d27227444f48dd4110ae0d0
Alexandr Smirnov <buyer@mail.ru>
2016-03-13 15:50:45 +0500

    init.sh - univresal user. hello.py - print python version.

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 import urllib
+import sys
 
 def app(environ, start_response):
     #data = b"Hello, World!\n"
     query_str = environ["QUERY_STRING"]
     reqs = query_str.split('&')
     reqs1 = [urllib.unquote(x) + "\n\r" for x in reqs]
-    data = "".join(reqs1)
+    data = "Python version: " + (sys.version) + "\n\r"
+    data += "".join(reqs1)
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
