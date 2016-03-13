c28982e6cc0d16e7f040ab5ece203b8489d91575
Alexandr Smirnov <buyer@mail.ru>
2016-03-13 05:12:09 +0000

    Configured models.py for 'qa'. Set tables

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(blank=True)
+	rating = models.IntegerField()
+	author = models.ForeignKey(User)
+	likes = models.TextField()
+
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(blank-True)
+	question = models.ForeignKey(Question)
+	author = models.ForeignKey(User)
+
