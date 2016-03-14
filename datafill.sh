97a49e0b2f91a98ada8dbbe6454a63a3e25ee1d2
Alexandr Smirnov <buyer@mail.ru>
2016-03-14 17:45:39 +0500

    datafill.sh: add fill datetime column 'added_at'

--- datafill.sh
+++ datafill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in `seq 1 100`;
 do
-	mysql -uroot -e "INSERT INTO answer_db.question_post (title, text,  author_id) VALUES ('title$i', 'query_post_text$i',  '1');"
+	mysql -uroot -e "INSERT INTO answer_db.question_post (title, text,  author_id, added_at) VALUES ('title$i', 'query_post_text$i',  '1', NOW());"
 done
